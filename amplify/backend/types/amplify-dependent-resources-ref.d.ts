127e6e62c1a86810be90f6f4d3259bb528ece6f0
juanf28 <juan-felipe-00@hotmail.com>
2023-05-26 12:54:19 -0500

    commit sql12 8.033

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,36 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "apie8d78426": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "auth": {
+    "juanmcell": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "juanmcell0a134c26": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "juanmcellb0228165": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const mysql = require('mysql12')
+const mysql = require('mysql2')
 const myconn = require('express-myconnection')
 
 const routes = require('./ApiAccesorios/routes')
